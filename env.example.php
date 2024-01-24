707674872d0473c3cd0743e255cd2a15792a643d
TranVanHoaiThuong <thuong10t2678@gmail.com>
2024-01-24 20:46:17 +0700

    Thêm env example

---
+++ env.example.php
@@ -0,0 +1,10 @@
+<?php
+// DB info
+define('DB_HOST', 'localhost');
+define('DB_PORT', '1433');
+define('DB_DATABASE', 'dbname');
+define('DB_USERNAME', 'username');
+define('DB_PASSWORD', 'asd@123');
+
+// Enviroment info
+define('URL', 'http://localhost');
